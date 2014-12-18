9823a2a09232806df576b899ea065722fb3d2991
Hays Clark <hays@infinitedescent.com>
2014-12-18 10:30:06 -0800

    cleaning up logic, only using haxelib in project.xml

---
+++ README.md
@@ -0,0 +1,12 @@
+Game Center Sample
+==================
+
+Setup
+-----
+
+For iOS run
+* run ```haxelib run lime rebuild gamecenter ios```
+
+For Mac run
+* run ```haxelib run lime rebuild gamecenter mac```
+
--- Source/Main.hx
+++ Source/Main.hx
@@ -1,36 +1,40 @@
 package;
 
-
 import openfl.display.Sprite;
 import openfl.text.TextField;
+import openfl.text.TextFieldType;
 import extension.gamecenter.GameCenter;
 import extension.gamecenter.GameCenterEvent;
 
 class Main extends Sprite {
 	
 	private var _tf:TextField;
+	
 	public function new () {
 		
 		super ();
 		
 		// Add TextField
+		addTextField();
+		
+        // Connect to GameCenter
+        connectToGameCenter();
+	}
+
+	private function addTextField():Void {
 		_tf = new TextField();
 		_tf.width = 200;
+		_tf.selectable = false;
+		_tf.type = TextFieldType.DYNAMIC;
         addChild(_tf);
-        
-        // Connect to GameCenter
-        if(!GameCenter.available) {
-            _tf.text = "GameCenter not available. :(";
-        } else {
-            getPlayerName();
-        }
 	}
-	
-	private function getPlayerName():Void {
-         GameCenter.addEventListener(GameCenterEvent.AUTH_SUCCESS, onGC_authSuccess); 
-		 GameCenter.addEventListener(GameCenterEvent.AUTH_FAILURE, onGC_authFailure);
 
-		 GameCenter.authenticate();
+	private function connectToGameCenter():Void {
+		_tf.text = "Connecting...";
+        GameCenter.addEventListener(GameCenterEvent.AUTH_SUCCESS, onGC_authSuccess); 
+		GameCenter.addEventListener(GameCenterEvent.AUTH_FAILURE, onGC_authFailure);
+
+		GameCenter.authenticate();
     }
 
 	private function onGC_authSuccess():Void {
